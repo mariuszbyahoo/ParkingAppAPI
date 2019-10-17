0bcc9311c061e1eca9c866fdbdabade1b025972e
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-17 10:04:39 +0200

    Add project files.

---
+++ ParkingAppAPI/Models/Slot.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParkingAppAPI
+{
+    public class Slot
+    {
+        public int posX { get; set; }
+        public int posY { get; set; }
+        public bool IsOccupied { get; set; }
+    }
+}
