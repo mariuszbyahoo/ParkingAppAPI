14acb7f95167b143e33d5fc106fe0288c0149b22
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-30 20:26:05 +0100

    Adding style and script

---
+++ ParkingAppAPI/wwwroot/script.js
@@ -0,0 +1,28 @@
+﻿(function attachColor()
+{
+    for (let i = 0; i < document.getElementsByClassName('slot').length; i++)
+    {
+        if (document.getElementsByClassName('slot').item(i).innerHTML === 'False')
+        {
+            document.getElementsByClassName('slot').item(i).style.backgroundColor = 'lawnGreen';
+        }
+        else
+        {
+            document.getElementsByClassName('slot').item(i).style.backgroundColor = 'red';
+        }
+    }
+})();
+
+// funkcja poniżej nie działa...
+
+function changeColor()
+{
+    if (this.InnerHtml === 'False') {
+        this.backgroundColor = 'red';
+        this.InnerHtml = 'True';
+    }
+    else {
+        this.backgroundColor = 'lawnGreen';
+        this.InnerHtml = 'False';
+    }
+}
