5ed60ec27dd2c7061c24ed451818d62bcc919310
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-11-23 12:21:06 +0100

    Implementing IComparable<Slot> and refactoring of style.css

--- ParkingAppAPI/Models/Slot.cs
+++ ParkingAppAPI/Models/Slot.cs
@@ -4,11 +4,15 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace ParkingApp.API
 {
-    public class Slot
+    public class Slot : IComparable<Slot>
     { 
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public bool IsOccupied { get; set; } = false;
 
+        public int CompareTo(Slot obj)
+        {
+            return ((IComparable)Id).CompareTo(obj.Id);
+        }
     }
 }
